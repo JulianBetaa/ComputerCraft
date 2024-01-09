8230bcf03a6f4ebdb4bb7c7ac1f4a1db6c471602
JulianBetaa <sievertjulian@gmail.com>
2024-01-09 22:15:37 +0100

    first commit

---
+++ Turtle/mineCube.lua
@@ -0,0 +1,37 @@
+local gangLaenge = 40
+local gangAnzahl = 10
+
+function main(gangLaenge, gangAnzahl)
+    for i = 1, gangAnzahl do
+        grabGang(gangLaenge)
+        turnAround()
+        move(gangLaenge)
+    end
+end
+function move(laenge)
+    for i = 1, laenge do
+        turtle.forward()
+    end
+    
+    
+end
+function grabGang(gangLaenge)
+    for i = 1, gangLaenge do
+        digAndMoveForward()
+    end
+end
+
+function digAndMoveForward()
+    while turtle.detect() do
+        turtle.dig()
+        sleep(0.5)
+    end
+    turtle.forward()
+    turtle.digUp()
+end
+function turnAround()
+    turtle.turnLeft()
+    turtle.turnLeft()
+end
+
+main(gangLaenge, gangAnzahl)
